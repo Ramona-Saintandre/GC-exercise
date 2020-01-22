2fff060bc35e86998c5541ffb4d6b80d43693cf6
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-22 18:52:26 -0500

    added interface for array

---
+++ typescript-tutorial/filterByTerm.ts
@@ -0,0 +1,14 @@
+interface ILink {
+  url: string;
+}
+
+function filterByTerm(input:Array<Ilink>, searchTerm:string) {
+    if (!searchTerm) throw Error("searchTerm cannot be empty");
+    if (!input.length) throw Error("inputArr cannot be empty");
+    const regex = new RegExp(searchTerm, "i");
+    return input.filter(function(arrayElement) {
+      return arrayElement.url.match(regex);
+    });
+  }
+  
+  filterByTerm([{url:'string1'},{url:'string2'},{url:'string3']},'java');
