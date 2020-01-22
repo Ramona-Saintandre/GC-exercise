1224c8d4f340774171b4d119b79e8679127fa835
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-22 18:58:32 -0500

    finished part two

--- typescript-tutorial/filterByTerm.js
+++ typescript-tutorial/filterByTerm.js
@@ -3,10 +3,15 @@ function filterByTerm(input, searchTerm) {
     if (!searchTerm)
         throw Error("searchTerm cannot be empty");
     if (!input.length)
-        throw Error("inputArr cannot be empty");
+        throw Error("input cannot be empty");
     var regex = new RegExp(searchTerm, "i");
     return input.filter(function (arrayElement) {
         return arrayElement.url.match(regex);
     });
 }
-filterByTerm([{ url: 'string1' }, { url: 'string2' }, { url: 'string3' }], 'java');
+var obj1 = { url: "string1" };
+var obj2 = { url: "string2" };
+var obj3 = { url: "string3" };
+var arrOfLinks = [obj1, obj2, obj3];
+var term = "java";
+filterByTerm(arrOfLinks, term);
