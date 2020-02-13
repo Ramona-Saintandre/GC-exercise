e4d2cf7b4065ef766f990b30ba71722a3a738d29
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-12 21:55:38 -0500

    created an array still need to declare

--- nodejs/server.js
+++ nodejs/server.js
@@ -6,3 +6,6 @@ res.writeHead(200, {"Content-type": "text/plain"});
 res.write("This is Ramona's first Node server");
 res.end();
 }).listen(8888);
+
+let monaFacts =['crazy','tired','german','grandma','old']
+ document.write(monaFacts);   
