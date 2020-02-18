31a011b423b2d9f79c081eaea66e6c03f0de45b1
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-17 19:00:57 -0500

    added facts.js file

--- nodejs/express/server.js
+++ nodejs/express/server.js
@@ -1,11 +1,20 @@
 // server.js
-"use strict";
-const http = require("http");
-http.createServer((req, res) => {
-res.writeHead(200, {"Content-type": "text/plain"});
-res.write("This is Ramona's first Node server");
-res.end();
-}).listen(8888);
+// "use strict";
+// const http = require("http");
+// http.createServer((req, res) => {
+// res.writeHead(200, {"Content-type": "text/plain"});
+// res.write("This is Ramona's first Node server");
+// res.end();
+// }).listen(8888);
 
-let monaFacts =['crazy','tired','german','grandma','old']
- document.write(monaFacts);   
+// let monaFacts =['crazy','tired','german','grandma','old']
+//  document.write(monaFacts);   
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () =>{
+    console.log('listening on htttp://lochost:${port}')
+})
